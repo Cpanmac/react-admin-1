02343ea7b5665c2ef66502a4008e7befe92d1434
wangxiaogang952 <wangxiaogang952@pingan.com.cn>
2018-04-28 19:49:24 +0800

    add readme axios config

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# back_office_management_system_framework
-react+router+redux+antd 后台管理系统框架 
+# react+router+redux+antd 后台管理系统框架
+
+    业务代码放在src/client下面
+
+    /app 是外层容器
+
+    /business 是放置复杂的业务逻辑（简单的业务逻辑在action中可以完成就无需放在此处
+        权限管理模块放置在此文件夹中
+
+    /common 放置公共配置，数据，业务等
+
+    /iconfont 是图标文件夹
+
+    /image 图片文件夹
+
+    /layout 系统的UI布局，包括头部，脚部，菜单栏，登陆页面
+
+    /mock mock数据文件夹
+
+    /pages  业务页面，存放每个页面的style，action，reducer，components
+
+    /redux 主要放置redux自动注入脚本和暴露对象配置
+
+    /route 路由配置
+
+    /service 调用API层
+
+    /styles css样式工具 各个页面的样式也是放在各个page下的
+
+    /utils 工具库
--- src/client/utils/axiosInit.js
+++ src/client/utils/axiosInit.js
@@ -2,10 +2,37 @@ import axios from 'axios'
 import React, {PureComponent, Component} from 'react'
 import { serverUrl } from '../common/config'
 
+// 服务端地址
 axios.defaults.baseURL = serverUrl;
+// 异步请求
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// json数据格式传输
 axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+// 跨域时是否带上cookie
 axios.defaults.withCredentials = true;
+// 请求超时时间配置(ms)
+axios.defaults.timeout = 20000;
+
+// 请求拦截器
+axios.interceptors.request.use(config => {
+    // 请求之前，搞些事情
+    return config;
+}, error => {
+    // 请求失败的时候，搞些事情
+    return Promise.reject(error);
+});
+
+// 响应拦截器
+axios.interceptors.response.use(response => {
+    // 服务端响应成功时，搞些事情
+    return response;
+}, error => {
+    // 服务端响应失败时，搞些事情
+    return Promise.reject(error);
+});
+
+
 
 let prop = {
     configurable: true,
@@ -13,7 +40,7 @@ let prop = {
     value: axios,
     writable: false
 };
-// 挂载到react对象和window对象上
+// 将axios挂载到react对象和window对象上
 Object.defineProperty(React, '$http', prop);
 Object.defineProperty(React, 'axios', prop);
 Object.defineProperty(window, 'axios', prop);
